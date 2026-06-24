c7a75efbb4f0c68bc0c8f99f2fc671a84b71fab1
BrianStovia <brianstovia@users.noreply.github.com>
2026-06-24 11:36:55 +0700

    perf: add net.core.somaxconn and net.ipv4.udp_mem sysctl kernel optimizations

--- install.sh
+++ install.sh
@@ -138,6 +138,8 @@ sysctl_optimize "net.ipv4.tcp_tw_reuse" "1"
 sysctl_optimize "net.core.netdev_max_backlog" "10000"
 sysctl_optimize "net.ipv4.udp_rmem_min" "8192"
 sysctl_optimize "net.ipv4.udp_wmem_min" "8192"
+sysctl_optimize "net.core.somaxconn" "32768"
+sysctl_optimize "net.ipv4.udp_mem" "114112 152152 228224"
 sysctl -p
 mkdir -p /etc/sysctl.d
 cat > /etc/sysctl.d/99-vpn.conf << EOF
@@ -162,6 +164,8 @@ net.ipv4.tcp_tw_reuse = 1
 net.core.netdev_max_backlog = 10000
 net.ipv4.udp_rmem_min = 8192
 net.ipv4.udp_wmem_min = 8192
+net.core.somaxconn = 32768
+net.ipv4.udp_mem = 114112 152152 228224
 EOF
 sysctl --system
 
